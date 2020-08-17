16f24f4ddadaa0ddc58300cc3fc854b3c334197e
heerock <heerock93@gmail.com>
2020-08-17 18:37:50 +0900

    Add ignore

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://youtube:xogml12!@youtube-clone.kqaxk.mongodb.net/test?retryWrites=true&w=majority', {
+
+mongoose.connect('mongodb+srv://boiler:xogml12!@youtube-clone.kqaxk.mongodb.net/boiler?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
@@ -12,6 +21,21 @@ mongoose.connect('mongodb+srv://youtube:xogml12!@youtube-clone.kqaxk.mongodb.net
 
 
 app.get('/', (req, res) => res.send('Hello World!!'))
+
+app.post('/register', (req, res) => {
+
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면 
+    // 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
